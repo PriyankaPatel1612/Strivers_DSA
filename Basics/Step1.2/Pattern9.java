b64f8be3b3b071003950b860f8b7c5abdbd3e94e
Priyanka Patel <priya.priyanka1612@gmail.com>
2024-03-19 16:00:10 +0530

    Diamond Star Pattern

---
+++ Basics/Step1.2/Pattern9.java
@@ -0,0 +1,50 @@
+/*
+
+   *
+  ***
+ *****
+ *****
+  ***
+   *
+   * 
+ */
+
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = scan.nextInt();
+        printPattern(n);
+    }
+
+    static void printPattern(int n){
+        
+        //first half
+        for(int row=1 ; row<=n ; row++){
+            //spaces
+            for(int sp=1 ; sp<=n-row ; sp++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int st=1 ; st<=2*row-1 ; st++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //next half
+        for(int row=n ; row>=1 ; row--){
+            //spaces
+            for(int sp=1 ; sp<=n-row ; sp++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int st=1 ; st<=2*row-1 ; st++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
