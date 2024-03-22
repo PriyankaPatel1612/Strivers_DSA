58617a6ccca41d4de0358b367a0ecdc234c1ad3c
Priyanka Patel <priya.priyanka1612@gmail.com>
2024-03-22 14:16:58 +0530

    added spaces

--- Basics/Step1.2/Pattern19.java
+++ Basics/Step1.2/Pattern19.java
@@ -24,15 +24,15 @@ public class Pattern19 {
         for(int row=1 ; row<=n ; row++){    
            //stars
            for(int st1=1 ; st1<=n+1-row ; st1++){
-            System.out.print("*");
+            System.out.print("* ");
            }
            //spaces
            for(int sp=1 ; sp<=2*row-2 ; sp++){
-            System.out.print(" ");
+            System.out.print("  ");
            }
            //stars
            for(int st2=1 ; st2<=n+1-row ; st2++){
-            System.out.print("*");
+            System.out.print("* ");
            }
            System.out.println();
         }
@@ -42,15 +42,15 @@ public class Pattern19 {
         for(int row=1 ; row<=n ; row++){    
            //stars
            for(int st1=1 ; st1<=row ; st1++){
-            System.out.print("*");
+            System.out.print("* ");
            }
            //spaces
            for(int sp=1 ; sp<=temp ; sp++){
-            System.out.print(" ");
+            System.out.print("  ");
            }
            //stars
            for(int st2=1 ; st2<=row ; st2++){
-            System.out.print("*");
+            System.out.print("* ");
            }
            System.out.println();
            temp-=2;
