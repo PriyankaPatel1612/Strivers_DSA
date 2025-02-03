fe1f85b4ac087a118938e2b69dd3210464ac446e
Priyanka Patel <priya.priyanka1612@gmail.com>
2025-02-03 22:22:02 +0530

    BubbleSort

--- Sorting Techniques/BubbleSort.java
+++ Sorting Techniques/BubbleSort.java
@@ -20,6 +20,20 @@ class BubbleSort{
             }
         }
 
+        /*
+        for(int i=0; i<arr.length-1; i++) {
+           for(int j=0; j<arr.length-i-1; j++) {
+               if(arr[j] > arr[j+1]) {
+                   //swap
+                   int temp = arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+               }
+           }
+       }
+
+         */
+
         System.out.println();
         System.out.println("After selection sort:");
         for (int i = 0; i < arr.length; i++) {
